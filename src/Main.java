3abb4dce155e45b2ecad3e95cba23e7a471f09bc
Oseeva <fselti1168rus@gmail.com>
2022-12-09 02:27:33 +0700

    Начальный комит

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int cat = 4000;
+        System.out.println("Значение переменной кошка с типом int равно " + cat);
+        byte dog = 2;
+        System.out.println("Значение переменной собака с типом byte равно " + dog);
+        short apple = 150;
+        System.out.println("Значение переменной яблоко с типом short равно " + apple);
+        long flower = 8400000;
+        System.out.println("Значение переменной цветок с типом long равно " + flower);
+        float owl = 1.5f;
+        System.out.println("Значение переменной сова с типом float равно " + owl);
+        double cake = 6.00001;
+        System.out.println("Значение переменной торт с типом double равно " + cake);
+
+    }
+}
